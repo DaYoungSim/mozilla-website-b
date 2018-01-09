7cd20139b5d7363ffe19ddebad93c3c37c13f3ab
DaYoung Sim <das9899@student.hks.harvard.edu>
2018-01-09 15:59:13 -0500

    add a new function Woof

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,14 @@ function sayOuch() {
     alert('Ouch! Stop poking me!');
 }
 
+function sayWoof() {
+    alert('Woof!');
+}
 // Use a CSS selector to identify an element
-var foxImage = document.querySelector('img');
+var foxImage = document.querySelector('img'); /*only selects the first image*/
+var foxImage = document.querySelector('#foxpic'); 
+var dogImage = document.querySelector('#dogpic'); 
 
 // Assign the function to the onclick event on that element
 foxImage.onclick = sayOuch;
+dogImage.onclike = sayWoof;
